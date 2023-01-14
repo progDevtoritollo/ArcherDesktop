c08f01545ca5e08d3a5b91515045ab47d91112b7
Igor Marusych <progdevtoritollo@gmail.com>
2023-01-14 22:12:25 +0200

    format

--- src/entities/ui/eventCard/index.tsx
+++ src/entities/ui/eventCard/index.tsx
@@ -1,120 +1,148 @@
-import React from "react";
+import React from 'react';
 import { UserOutlined } from '@ant-design/icons';
 import { Avatar } from 'antd';
 
-import gold_trophy from "app/assets/img/gold_trophy.png";
-import bronze_trophy from "app/assets/img/bronze_trophy.png";
-import silver_trophy from "app/assets/img/silver_trophy.png";
-import "./index.scss"
-
+import gold_trophy from 'app/assets/img/gold_trophy.png';
+import bronze_trophy from 'app/assets/img/bronze_trophy.png';
+import silver_trophy from 'app/assets/img/silver_trophy.png';
+import './index.scss';
 
 interface CardEventProps {
-  name: string
-  event: string
-  time: string
-  userScore: number,
-  opponentScore?: number,
-  opponentName?: string,
-  competitionPlace?: number,
-  userPhoto?: string
+	name: string;
+	event: string;
+	time: string;
+	userScore: number;
+	opponentScore?: number;
+	opponentName?: string;
+	competitionPlace?: number;
+	userPhoto?: string;
 }
 
+const CardEvent = ({ name, event, opponentScore = 0, time, userScore, opponentName, userPhoto, competitionPlace }: CardEventProps) => {
+	switch (event) {
+		case 'check':
+			return (
+				<div className='card'>
+					<div className='left'>
+						<div className='card__avatar'>
+							<Avatar
+								src={userPhoto}
+								size='large'
+								icon={<UserOutlined />}
+							/>
+						</div>
 
-function CardEvent({ name, event, opponentScore = 0, time, userScore, opponentName, userPhoto, competitionPlace }: CardEventProps) {
+						<div className='text'>
+							<div className='card__name'>{name}</div>
+							<div className='card__description'>check</div>
+						</div>
+					</div>
+					<div className='right'>
+						<div className='card__check-score'>{userScore}/300</div>
+						<div className='card__time'>{time}</div>
+					</div>
+				</div>
+			);
+		case 'duel':
+			return (
+				<div className='card'>
+					<div className='left'>
+						<div className='card__avatar'>
+							<Avatar
+								src={userPhoto}
+								size='large'
+								icon={<UserOutlined />}
+							/>
+						</div>
+						<div className='text'>
+							<div className='card__name'>{name}</div>
+							<div className='card__description'>
+								duel with
+								{' ' + opponentName}
+							</div>
+						</div>
+					</div>
+					<div className='right'>
+						{userScore <= opponentScore ? (
+							<div className='card__duel-score card__duel-score_lose'>
+								{userScore}:{opponentScore}
+							</div>
+						) : (
+							<div className='card__duel-score card__duel-score_win'>
+								{userScore}:{opponentScore}
+							</div>
+						)}
+						<div className='card__time'>{time}</div>
+					</div>
+				</div>
+			);
+		case 'competition':
+			return (
+				<div className='card'>
+					<div className='left'>
+						<div className='card__avatar'>
+							<Avatar
+								src={userPhoto}
+								size='large'
+								icon={<UserOutlined />}
+							/>
+						</div>
+						<div className='text'>
+							<div className='card__name'>{name}</div>
+							<div className='card__description'>{event}</div>
+						</div>
+					</div>
 
-  switch (event) {
-    case "check":
-      return (
-        <div className="card">
-          <div className="left">
-            <div className="card__avatar">
-              <Avatar src={userPhoto} size="large" icon={<UserOutlined />} />
-            </div>
-            <div className="text">
-              <div className="card__name" >{name}</div>
-              <div className="card__description">check</div></div>
-          </div>
-          <div className="right">
-            <div className="card__check-score">{userScore}/300</div>
-            <div className="card__time">{time}</div>
-          </div>
-        </div>
-      )
-    case "duel":
-      return (
-        <div className="card">
-          <div className="left"><div className="card__avatar">
-            <Avatar src={userPhoto} size="large" icon={<UserOutlined />} />
-          </div>
-            <div className="text">
-              <div className="card__name" >{name}</div>
-              <div className="card__description" >duel with {opponentName}</div>
-            </div>
-          </div>
-          <div className="right">
-            {
-              userScore <= opponentScore ?
-                <div className="card__duel-score card__duel-score_lose">{userScore}:{opponentScore}</div> :
-                <div className="card__duel-score card__duel-score_win">{userScore}:{opponentScore}</div>
-            }
-            <div className="card__time">{time}</div>
-          </div>
-        </div>
-      )
-    case "competition":
-      return (
-        <div className="card">
-          <div className="left">
-            <div className="card__avatar">
-              <Avatar src={userPhoto} size="large" icon={<UserOutlined />} />
-            </div>
-            <div className="text">
-              <div className="card__name">{name} </div>
-              <div className="card__description">{event}</div>
-            </div>
-          </div>
+					<div className='right'>
+						<div className='card__competition-place'>
+							{competitionPlace === 1 ? (
+								<img
+									src={gold_trophy}
+									alt='trophy'></img>
+							) : competitionPlace === 2 ? (
+								<img
+									src={silver_trophy}
+									alt='trophy'></img>
+							) : competitionPlace === 3 ? (
+								<img
+									src={bronze_trophy}
+									alt='trophy'></img>
+							) : (
+								<div className='number'>
+									Place#
+									{competitionPlace}
+								</div>
+							)}
+						</div>
+						<div className='card__time'>{time}</div>
+					</div>
+				</div>
+			);
+		default:
+			return <div>error</div>;
+	}
 
-          <div className="right">
-            <div className="card__competition-place">
-              {
-                competitionPlace === 1 ? <img src={gold_trophy} alt="trophy"></img> : competitionPlace === 2 ? <img src={silver_trophy} alt="trophy"></img> :
-                  competitionPlace === 3 ? <img src={bronze_trophy} alt="trophy"></img> :
-                    <div className="number">Place#{competitionPlace}</div>
-              }
-            </div>
-            <div className="card__time">{time}</div>
-          </div>
-        </div>
-      );
-    default:
-      return (
-        <div>error</div>
-      )
-  }
+	//* competition
 
+	// duel
+	// return (
+	// 	<div className="card">
+	// 		<div className="card__name" >Игорь Марусич </div>
+	// 		<div className="card__description" >competition</div>
+	// 		<div className="card__info-box">gold cup</div>
+	// 		<div className="card__time">3 min</div>
+	// 	</div>
+	// )
 
-  // competition
-
-
-  // duel
-  // return (
-  // 	<div className="card">
-  // 		<div className="card__name" >Игорь Марусич </div>
-  // 		<div className="card__description" >competition</div>
-  // 		<div className="card__info-box">gold cup</div>
-  // 		<div className="card__time">3 min</div>
-  // 	</div>
-  // )
-
-  //check
-  // return (
-  // 	<div className="card">
-  // 		<div className="card__name" >Игорь Марусич </div>
-  // 		<div className="card__description" >competition</div>
-  // 		<div className="card__info-box">gold cup</div>
-  // 		<div className="card__time">3 min</div>
-  // 	</div>
-  // )
-}
+	//check
+	// return (
+	// 	<div className="card">
+	// 		<div className="card__name" >Игорь Марусич </div>
+	// 		<div className="card__description" >competition</div>
+	// 		<div className="card__info-box">gold cup</div>
+	// 		<div className="card__time">3 min</div>
+	// 	</div>
+	// )
+};
 
 export default CardEvent;
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,21 +1,27 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-import LoginForm from "features/auth/LoginForm/LoginForm";
-import RegisterForm from "features/auth/RegisterForm/RegisterForm";
-import "./Auth.scss";
+import LoginForm from 'features/auth/LoginForm/LoginForm';
+import RegisterForm from 'features/auth/RegisterForm/RegisterForm';
+import './Auth.scss';
 
 const Auth = () => {
-  return (
-    <section className="auth">
-      <div className="auth__content">
-        <Routes>
-          <Route path="/signin" element={<LoginForm />} />
-          <Route path="/signup" element={<RegisterForm />} />
-        </Routes>
-      </div>
-    </section>
-  );
+	return (
+		<section className='auth'>
+			<div className='auth__content'>
+				<Routes>
+					<Route
+						path='/signin'
+						element={<LoginForm />}
+					/>
+					<Route
+						path='/signup'
+						element={<RegisterForm />}
+					/>
+				</Routes>
+			</div>
+		</section>
+	);
 };
 
 export default Auth;
